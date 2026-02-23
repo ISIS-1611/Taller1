b2877f3040eaec21480b84ea0a6697ff45ccbfe3
Nicolas Arango <n.arangor@uniandes.edu.co>
2026-02-22 21:02:35 -0500

    time

--- SAR/algorithms/search.py
+++ SAR/algorithms/search.py
@@ -16,16 +16,6 @@ def tinyHouseSearch(problem: SearchProblem):
 def depthFirstSearch(problem: SearchProblem):
     """
     Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     
     print("Start:", problem.getStartState())
